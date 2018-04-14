8feae692b618f910510b14f0c8e9036130c40bfd
Ruud van Asseldonk <dev@veniogames.com>
2018-04-14 22:15:45 +0200

    Extract download into function

--- src/main.rs
+++ src/main.rs
@@ -8,33 +8,36 @@ use std::io::Write;
 use std::io;
 
 use futures::{Future, Stream};
-use hyper::{Client, Uri};
+use hyper::{Chunk, Client, Uri};
 use tokio_core::reactor::Core;
 
 mod config;
 mod error;
 
-fn download(core: &mut Core) -> Result<(), Box<::std::error::Error>> {
-    let url = ("https://hyper.rs").parse().unwrap();
-    let client = hyper::Client::configure()
-        .connector(hyper_rustls::HttpsConnector::new(4, &core.handle()))
-        .build(&core.handle());
-    let work = client.get(url).and_then(|res| {
-        println!("Response");
-        assert_eq!(res.status(), hyper::Ok);
-        res.body().for_each(|chunk| {
-            println!("Chunk");
-            io::stdout()
-                .write_all(&chunk)
-                .map(|_| ())
-                .map_err(From::from)
-        })
-    });
-    core.run(work)?;
-    Ok(())
+fn download(core: &Core, uri: &Uri) -> Box<Stream<Item = Chunk, Error = hyper::Error>> {
+    let num_dns_worker_threads = 4;
+    match uri.scheme() {
+        Some("https") => {
+            let client = hyper::Client::configure()
+                .connector(hyper_rustls::HttpsConnector::new(num_dns_worker_threads, &core.handle()))
+                .build(&core.handle());
+            // TODO: Verify that the response status code is 200 OK.
+            // TODO: Deal with 301 Moved, but take care not to end in a loop.
+            Box::new(client.get(uri.clone()).map(|res| res.body()).flatten_stream())
+        }
+        Some(scheme) => panic!("Unsupported scheme {}.", scheme),
+        None => panic!("Invalid url, must include http/https scheme."),
+    }
 }
 
 fn main() {
     let mut core = Core::new().unwrap();
-    download(&mut core).unwrap();
+    let url = ("https://hyper.rs").parse().unwrap();
+    let work = download(&core, &url);
+    core.run(work.for_each(|chunk| {
+        io::stdout()
+            .write_all(&chunk)
+            .map(|_| ())
+            .map_err(From::from)
+    })).unwrap();
 }
