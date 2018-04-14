1689b057f9c62fe917f9f31f45826626e589286d
Ruud van Asseldonk <dev@veniogames.com>
2018-04-14 17:35:16 +0200

    Implement simple http over tls client
    Dowloading a page over https, in a single static executable, yay!

--- README.md
+++ README.md
@@ -5,5 +5,5 @@ Tako: take container image.
 ## Building
 
     rustup target add x86_64-unknown-linux-musl
-    cargo build --release --target=x86_64-unknown-linux-musl
+    cargo build --release
     target/x86_64-unknown-linux-musl/release/tako
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+extern crate hyper;
+extern crate hyper_rustls;
+extern crate tokio_core;
+extern crate futures;
+
+use hyper::{Client, Uri};
+use futures::{Future, Stream};
+use tokio_core::reactor::Core;
+use std::io;
+use std::io::Write;
+
+fn download(core: &mut Core) -> Result<(), Box<::std::error::Error>> {
+    let url = ("https://hyper.rs").parse().unwrap();
+    let client = hyper::Client::configure()
+        .connector(hyper_rustls::HttpsConnector::new(4, &core.handle()))
+        .build(&core.handle());
+    let work = client.get(url).and_then(|res| {
+        println!("Response");
+        assert_eq!(res.status(), hyper::Ok);
+        res.body().for_each(|chunk| {
+            println!("Chunk");
+            io::stdout()
+                .write_all(&chunk)
+                .map(|_| ())
+                .map_err(From::from)
+        })
+    });
+    core.run(work)?;
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut core = Core::new().unwrap();
+    download(&mut core).unwrap();
 }
