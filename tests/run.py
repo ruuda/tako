bdae5824e0fa05b2f2994ec838adf93a1e972f6b
Ruud van Asseldonk <dev@veniogames.com>
2018-04-19 14:33:33 +0200

    Fix and extend tests, make them less verbose

--- tests/run.py
+++ tests/run.py
@@ -6,6 +6,7 @@ import os.path
 import shutil
 import socketserver
 import subprocess
+import sys
 import threading
 
 is_repo_root = os.path.exists(os.path.join(os.getcwd(), 'README.md'))
@@ -13,14 +14,21 @@ assert is_repo_root, 'This script must be run from the root of the repository.'
 
 
 def exec(args):
-    subprocess.run(args).check_returncode()
+    p = subprocess.run(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    if p.returncode != 0:
+        print('Process {} exited with nonzero exit code.'.format(args))
+        print('\nSTDOUT\n------')
+        sys.stdout.buffer.write(p.stdout)
+        print('\nSTDERR\n------')
+        sys.stdout.buffer.write(p.stderr)
+        sys.exit(-1)
 
 
 def run_server():
     port = 8117
     Handler = http.server.SimpleHTTPRequestHandler
-    httpd = socketserver.TCPServer(('', port), Handler)
-    httpd.serve_forever()
+    with socketserver.TCPServer(('', port), Handler) as httpd:
+        httpd.serve_forever()
 
 
 # Run the http server in a background thread. Don't wait for the
@@ -31,11 +39,17 @@ httpd_thread.start()
 # Clean up results from a previous test run, if there are any.
 shutil.rmtree('tests/scratch')
 os.mkdir('tests/scratch')
+os.mkdir('tests/scratch/foo')
 
 # Print a backtrace if the Rust program crashes.
 os.environ['RUST_BACKTRACE'] = '1'
 
 print('tako fetch')
 
-print(' * fetches the manifest')
+print(' * fetches the manifest into an empty destination')
 exec(['target/debug/tako', 'fetch', 'tests/config/foo.tako'])
+assert os.path.exists('tests/scratch/foo/manifest')
+
+print(' * fetches the manifest into an non-empty destination')
+exec(['target/debug/tako', 'fetch', 'tests/config/foo.tako'])
+assert os.path.exists('tests/scratch/foo/manifest')
