db0b507598daee6f4659c26d8c2b04a90d2de2ea
Ruud van Asseldonk <dev@veniogames.com>
2018-05-20 01:03:14 +0200

    Make output of test script TAP compliant
    The TAP output is not terribly useful in itself, but as a side effects,
    the script now does not terminate early when an assertion fails; it
    still runs the remaining tests. Also, it now prints details to stdout,
    so silencing stderr is no longer such a big problem.
    
    Using a context manager for this is quite nice: you can capture the test
    name in advance, print it afterwards, and still handle any exceptions
    that occur the the body. Python is nice :)

--- tests/run.py
+++ tests/run.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3
 
+"""
+A basic Python script that starts a server and tests a few properties by running
+Tako. Test output is TAP compliant (see https://testanything.org), but the
+script can also be used standalone.
+"""
+
+import contextlib
 import http.server
 import os
 import os.path
@@ -8,22 +15,63 @@ import socketserver
 import subprocess
 import sys
 import threading
+import traceback
+
+# Print the number of tests in advance. This is understood by the TAP protocol,
+# and used to recognize failure when the script unexpectedly exits early.
+print('1..7')
 
 is_repo_root = os.path.exists(os.path.join(os.getcwd(), 'README.md'))
 assert is_repo_root, 'This script must be run from the root of the repository.'
 
+test_number = 0
+test_failed = False
+
+class ExecError(Exception):
+    """ Raised from exec(), used to print stderr after test description. """
+
+    def __init__(self, command, code, stdout, stderr):
+        self.command = command
+        self.code = code
+        self.stdout = stdout
+        self.stderr = stderr
+
+    def print_details(self):
+        print('# Process {} exited with unexpected '
+              'exit code {}.'.format(self.command, self.code))
+        print('\nSTDOUT\n------')
+        sys.stdout.flush()
+        sys.stdout.buffer.write(self.stdout)
+        print('\nSTDERR\n------')
+        sys.stdout.flush()
+        sys.stdout.buffer.write(self.stderr)
+        print('')
+
 
 def exec(*args, expect=0):
-    """ Run a program with an expected exit code, print stdout on mismatch. """
+    """ Run a program with an expected exit code, raise error on mismatch. """
     p = subprocess.run(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     if p.returncode != expect:
-        print('Process {} exited with unexpected '
-              'exit code {}.'.format(args, p.returncode))
-        print('\nSTDOUT\n------')
-        sys.stdout.buffer.write(p.stdout)
-        print('\nSTDERR\n------')
-        sys.stdout.buffer.write(p.stderr)
-        sys.exit(-1)
+        raise ExecError(args, p.returncode, p.stdout, p.stderr)
+
+
+@contextlib.contextmanager
+def test(description):
+    """ Context manager to run a TAP test. Use regular assert/raise inside. """
+    global test_number, test_failed
+    test_number = test_number + 1
+    try:
+        yield
+    except:
+        print('not ok {} {}'.format(test_number, description))
+        exc_type, error, tb = sys.exc_info()
+        _filename, line, _function, statement = traceback.extract_tb(tb)[-1]
+        print('# line {}: {}'.format(line, statement))
+        if exc_type is ExecError:
+            error.print_details()
+        test_failed = True
+    else:
+        print('ok {} {}'.format(test_number, description))
 
 
 def run_server():
@@ -60,64 +108,68 @@ os.mkdir('tests/scratch/bar-origin')
 # Print a backtrace if the Rust program crashes.
 os.environ['RUST_BACKTRACE'] = '1'
 
-print('tako store')
+print('\n# tako store\n')
 
 img_v1_sha = 'a18339e497c231154b9d06c809ef7e03a44cd59eb74217c64886b00696ce7062'
 
-print(' * stores into an empty server directory')
-exec('target/debug/tako', 'store',
-     '--key', secret_key,
-     '--output', 'tests/scratch/bar-origin',
-     'tests/images/1.0.0.img', '1.0.0')
-assert os.path.exists('tests/scratch/bar-origin/manifest')
-assert os.path.exists('tests/scratch/bar-origin/store/' + img_v1_sha)
+# print(' * stores into an empty server directory')
+with test('Stores into an empty server directory'):
+    exec('target/debug/tako', 'store',
+         '--key', secret_key,
+         '--output', 'tests/scratch/bar-origin',
+         'tests/images/1.0.0.img', '1.0.0')
+    assert os.path.exists('tests/scratch/bar-origin/manifest')
+    assert os.path.exists('tests/scratch/bar-origin/store/' + img_v1_sha)
 
-print('tako fetch')
+print('\n# tako fetch\n')
 
-print(' * fetches the manifest into an empty destination')
-exec('target/debug/tako', 'fetch', 'tests/config/foo-none.tako')
-assert os.path.exists('tests/scratch/foo/manifest')
+with test('Fetches the manifest into an empty destination'):
+    exec('target/debug/tako', 'fetch', 'tests/config/foo-none.tako')
+    assert os.path.exists('tests/scratch/foo/manifest')
 
-print(' * fetches the manifest into an non-empty destination')
-exec('target/debug/tako', 'fetch', 'tests/config/foo-none.tako')
-assert os.path.exists('tests/scratch/foo/manifest')
+with test('Fetches the manifest into an non-empty destination'):
+    exec('target/debug/tako', 'fetch', 'tests/config/foo-none.tako')
+    assert os.path.exists('tests/scratch/foo/manifest')
 
 img_v2_sha = '64358f43b990c1473817773028ff27029f4d367bf06595b6948d746fece678cd'
 foo_store_img_v2 = 'tests/scratch/foo/store/' + img_v2_sha
 store_img_v2 = 'store/' + img_v2_sha
 
-print(' * fetches an image into the destination store')
-exec('target/debug/tako', 'fetch', 'tests/config/foo-any.tako')
-assert os.path.exists('tests/scratch/foo/manifest')
-assert os.path.exists(foo_store_img_v2)
-# The files in the store must be readonly.
-assert not os.access(foo_store_img_v2, os.W_OK)
-assert os.readlink('tests/scratch/foo/latest') == store_img_v2
+with test('Fetches an image into the destination store'):
+    exec('target/debug/tako', 'fetch', 'tests/config/foo-any.tako')
+    assert os.path.exists('tests/scratch/foo/manifest')
+    assert os.path.exists(foo_store_img_v2)
+    # The files in the store must be readonly.
+    assert not os.access(foo_store_img_v2, os.W_OK)
+    assert os.readlink('tests/scratch/foo/latest') == store_img_v2
 
-print(' * does not download an existing image')
-exec('target/debug/tako', 'fetch', 'tests/config/foo-any.tako')
-# TODO: Add a hook to the webserver, and verify that indeed we did not get a
-# request for the image, only for the manifest.
-assert os.path.exists(foo_store_img_v2)
-assert os.readlink('tests/scratch/foo/latest') == store_img_v2
+with test('Does not download an existing image'):
+    exec('target/debug/tako', 'fetch', 'tests/config/foo-any.tako')
+    # TODO: Add a hook to the webserver, and verify that indeed we did not
+    # get a # request for the image, only for the manifest.
+    assert os.path.exists(foo_store_img_v2)
+    assert os.readlink('tests/scratch/foo/latest') == store_img_v2
 
-print(' * deletes a damaged image')
-# Corrupt the file in the store. Running "tako fetch" again should detect this,
-# and delete the file (such that on a next run it would be redownloaded).
-os.chmod(foo_store_img_v2, int('755', 8))
-with open(foo_store_img_v2, 'w') as f:
-    f.write('burrito')
-os.chmod(foo_store_img_v2, int('555', 8))
-# TODO: The expected exit code should be 1 for failure, not 101 (for panic).
-exec('target/debug/tako', 'fetch', 'tests/config/foo-any.tako', expect=101)
-assert not os.path.exists(foo_store_img_v2)
+with test('Deletes a damaged image'):
+    # Corrupt the file in the store. Running "tako fetch" again should
+    # detect this, and delete the file (such that on a next run it would
+    # be # redownloaded).
+    os.chmod(foo_store_img_v2, int('755', 8))
+    with open(foo_store_img_v2, 'w') as f:
+        f.write('burrito')
+    os.chmod(foo_store_img_v2, int('555', 8))
+    # TODO: The expected exit code should be 1 for failure, not 101 (for panic).
+    exec('target/debug/tako', 'fetch', 'tests/config/foo-any.tako', expect=101)
+    assert not os.path.exists(foo_store_img_v2)
 
-print(' * fetches a previously stored manifest')
-exec('target/debug/tako', 'fetch', 'tests/config/bar.tako')
-assert os.path.exists('tests/scratch/bar/manifest')
-assert os.readlink('tests/scratch/bar/latest') == 'store/' + img_v1_sha
+with test('Fetches a previously stored manifest'):
+    exec('target/debug/tako', 'fetch', 'tests/config/bar.tako')
+    assert os.path.exists('tests/scratch/bar/manifest')
+    assert os.readlink('tests/scratch/bar/latest') == 'store/' + img_v1_sha
 
 # TODO: Test that Tako follows redirects.
 # TODO: Test that Tako handles file-not-found correctly (whatever that means).
 
-print('All tests passed.')
+if test_failed:
+    print('Some tests failed.')
+    sys.exit(1)
