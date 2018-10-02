b69618d35cd874b03df75562d1105b3919673eb3
Ruud van Asseldonk <dev@veniogames.com>
2018-10-02 20:52:06 +0200

    Print last chunk as well in chunker
    Also decrease the threshold as we go over the ideal chunk size. This can
    improve sharing quite a lot (20% or so in my experiments), and it works
    better than just cutting at a certain maximum like casync does.

--- src/chunk.rs
+++ src/chunk.rs
@@ -7,27 +7,46 @@ use crc::crc16;
 use error::Result;
 use util;
 
+const MIN_CHUNK_LEN: usize = 512;
+const IDEAL_SIZE: usize = 2048;
+
+fn print_chunk(crc: u16, chunk: &[u8]) {
+    let digest = sha256::hash(chunk);
+    let mut digest_hex = String::new();
+    util::append_hex(&mut digest_hex, digest.as_ref());
+    println!("{:3} {:04x} {}", chunk.len(), crc, digest_hex);
+}
+
 fn split_buffer_into_chunks(data: &[u8]) {
     let mut crc = 1;
     let mut data_slice = data;
-    loop {
+    let mut has_more = true;
+
+    while has_more {
+        has_more = false;
+        let mut split_threshold = 0xffff / IDEAL_SIZE as u16;
+
         for (i, &b) in data_slice.iter().enumerate() {
             crc = crc16(crc, b);
-            println!("{} 0x{:02x} {:04x}", i, b, crc);
-            if crc & 0xfff == 0 {
+            if crc < split_threshold && i >= MIN_CHUNK_LEN {
                 let (chunk, remainder) = data_slice.split_at(i);
-                let digest = sha256::hash(chunk);
-                let mut digest_hex = String::new();
-                util::append_hex(&mut digest_hex, digest.as_ref());
-                println!("{} {} {:04x} {}", i, chunk.len(), crc, digest_hex);
+                print_chunk(crc, chunk);
                 assert!(data_slice.len() > remainder.len(), "{} > {}", data_slice.len(), remainder.len());
                 data_slice = remainder;
+                has_more = data_slice.len() > 0;
+                crc = 1;
                 break;
             }
+
+            // Increase the splitting probability as the chunk grows larger, to
+            // avoid very large chunks due to being unlucky. This also benefits
+            // chunk reuse.
+            if i >= IDEAL_SIZE {
+                split_threshold += 2;
+            }
         }
-        break;
-        if data_slice.len() == 0 {
-            break;
+        if !has_more {
+            print_chunk(crc, data_slice);
         }
     }
 }
