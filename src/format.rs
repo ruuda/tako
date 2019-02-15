c1a46c3007f756dbab6fc612cbd711d6b16b6240
Ruud van Asseldonk <dev@veniogames.com>
2019-02-15 22:44:06 +0100

    Manually implement base64 decoding

--- src/format.rs
+++ src/format.rs
@@ -5,6 +5,12 @@
 // you may not use this file except in compliance with the License.
 // A copy of the License has been included in the root of the repository.
 
+//! Implementations of base64 formatting.
+//!
+//! See [RFC 3548](https://tools.ietf.org/html/rfc3548) for reference.
+//! Especially the illustrations in section 7 are helpful when reading this
+//! implementation.
+
 const BASE64_CHARS: [char; 64] = [
     'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
     'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
@@ -37,11 +43,62 @@ pub fn append_base64(string: &mut String, bytes: &[u8]) {
     }
 }
 
+/// Return `i` such that `BASE64_CHARS[i] == ch`.
+fn decode_base64_char(ch: u8) -> Option<u8> {
+    match ch {
+        _ if b'A' <= ch && ch <= b'Z' => Some(ch - b'A'),
+        _ if b'a' <= ch && ch <= b'z' => Some(26 + (ch - b'a')),
+        _ if b'0' <= ch && ch <= b'9' => Some(52 + (ch - b'0')),
+        b'+' => Some(62),
+        b'/' => Some(63),
+        _ => None,
+    }
+}
+
+/// Decode a base64 (with + and /) string back to bytes.
+pub fn decode_base64(b64: &str) -> Option<Vec<u8>> {
+    // The input string length must be a multiple of 4.
+    let max_bytes_len = match b64.len() {
+        n if n % 4 != 0 => return None,
+        n => n / 4 * 3,
+    };
+
+    let mut bytes = Vec::with_capacity(max_bytes_len);
+
+    for quartet in b64.as_bytes().chunks(4) {
+        let b0 = decode_base64_char(quartet[0])?;
+        let b1 = decode_base64_char(quartet[1])?;
+        bytes.push((b0 << 2) | (b1 >> 4));
+
+        let b2 = match &quartet[2..4] {
+            b"==" if bytes.len() == max_bytes_len - 2 => return Some(bytes),
+            _ => decode_base64_char(quartet[2])?,
+        };
+
+        bytes.push((b1 & 0b00_11_11) << 4 | (b2 >> 2));
+
+        let b3 = match quartet[3] {
+            b'=' if bytes.len() == max_bytes_len - 1 => return Some(bytes),
+            k => decode_base64_char(k)?,
+        };
+
+        bytes.push((b2 & 0b00_00_11) << 6 | b3);
+    }
+
+    Some(bytes)
+}
+
 #[cfg(test)]
 mod test {
-    use base64;
+    use super::{append_base64, decode_base64};
 
-    use super::{append_base64};
+    #[test]
+    fn base64_slice_of_len_0_roundtrips() {
+        let mut s = String::new();
+        append_base64(&mut s, &[]);
+        assert_eq!(s, "");
+        assert_eq!(decode_base64("").unwrap(), &[]);
+    }
 
     #[test]
     fn base64_slice_of_len_1_roundtrips() {
@@ -49,8 +106,7 @@ mod test {
             let data = [i as u8];
             let mut s = String::new();
             append_base64(&mut s, &data);
-
-            assert_eq!(base64::decode(&s).unwrap(), &data);
+            assert_eq!(decode_base64(&s).unwrap(), &data);
         }
     }
 
@@ -61,8 +117,7 @@ mod test {
                 let data = [i as u8, j as u8];
                 let mut s = String::new();
                 append_base64(&mut s, &data);
-
-                assert_eq!(base64::decode(&s).unwrap(), &data);
+                assert_eq!(decode_base64(&s).unwrap(), &data);
             }
         }
     }
@@ -76,8 +131,7 @@ mod test {
                     let data = [i as u8, j as u8, k as u8];
                     let mut s = String::new();
                     append_base64(&mut s, &data);
-
-                    assert_eq!(base64::decode(&s).unwrap(), &data);
+                    assert_eq!(decode_base64(&s).unwrap(), &data);
                 }
             }
         }
@@ -93,11 +147,40 @@ mod test {
                         let data = [i as u8, j as u8, k as u8, l as u8];
                         let mut s = String::new();
                         append_base64(&mut s, &data);
-
-                        assert_eq!(base64::decode(&s).unwrap(), &data);
+                        assert_eq!(decode_base64(&s).unwrap(), &data);
                     }
                 }
             }
         }
+    }
+
+    #[test]
+    fn base64_decode_fails_on_invalid_length() {
+        assert!(decode_base64("a").is_none());
+        assert!(decode_base64("ab").is_none());
+        assert!(decode_base64("abc").is_none());
+        assert!(decode_base64("abc_f").is_none());
+        assert!(decode_base64("abc_fg").is_none());
+        assert!(decode_base64("abc_fgh").is_none());
+    }
+
+    #[test]
+    fn base64_decode_fails_on_invalid_characters() {
+        assert!(decode_base64("abc\0").is_none());
+        assert!(decode_base64("abc*").is_none());
+        assert!(decode_base64("abc.").is_none());
+        assert!(decode_base64("abc:").is_none());
+        assert!(decode_base64("abc@").is_none());
+        assert!(decode_base64("abc[").is_none());
+        assert!(decode_base64("abc{").is_none());
+    }
+
+    #[test]
+    fn base64_decode_fails_on_interior_padding() {
+        assert!(decode_base64("=123").is_none());
+        assert!(decode_base64("0=23").is_none());
+        assert!(decode_base64("01=3").is_none());
+        assert!(decode_base64("==23").is_none());
+        assert!(decode_base64("0==3").is_none());
     }
 }
